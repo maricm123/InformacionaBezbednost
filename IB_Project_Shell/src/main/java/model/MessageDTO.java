862a27a6f6048586313efe14ee84c0a22540e164
maricm123 <mihailomaric001@gmail.com>
2021-09-15 12:26:26 +0200

    Deleted data folder

---
+++ IB_Project_Shell/src/main/java/model/MessageDTO.java
@@ -0,0 +1,61 @@
+package model;
+
+
+
+import java.util.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import javax.xml.transform.dom.*;
+import org.w3c.dom.*;
+
+public class MessageDTO {
+	
+	private String emailAddress;
+	private String sender;
+	private String subject;
+	private String content;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	@Override
+	public String toString() {
+		return "MessageDTO [emailAddress=" + emailAddress + ", sender=" + sender + ", subject=" + subject + ", content="
+				+ content + "]";
+	}
+	public static MessageDTO loadFromDom(Node node) {
+		Element element=(Element) node;
+		MessageDTO m=new MessageDTO();
+		String posiljaoc=element.getAttribute("posiljaoc");
+		m.setSender(posiljaoc);
+		String primalac=element.getAttribute("primalac");
+		m.setEmailAddress(primalac);
+		String subject=element.getElementsByTagName("subject").item(0).getTextContent();
+		m.setSubject(subject);
+		String body=element.getElementsByTagName("body").item(0).getTextContent();
+		m.setContent(body);
+		return m;
+	}
+}
---
+++ IB_Project_Shell/src/main/java/model/Messages.java
@@ -0,0 +1,55 @@
+package model;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Messages {
+
+	private List<MessageDTO> messages=new ArrayList<MessageDTO>();
+	private boolean signature;
+
+	public boolean isSignature() {
+		return signature;
+	}
+
+	public void setSignature(boolean signature) {
+		this.signature = signature;
+	}
+
+	public List<MessageDTO> getMessages() {
+		return messages;
+	}
+
+	public void setMessageDTO(List<MessageDTO> messages) {
+		this.messages = messages;
+	}
+
+	@Override
+	public String toString() {
+		return "Messages [messages=" + messages + "]";
+	}
+	
+	public static Messages loadFromDom(Node node,String primalac) {
+		Messages messages=new Messages();
+		Element element=(Element) node;
+		NodeList nodeList=element.getElementsByTagName("poruka");
+		for(int i=0; i<nodeList.getLength(); i++) {
+			Node n=nodeList.item(i);
+			MessageDTO message=MessageDTO.loadFromDom(n);
+			messages.getMessages().add(message);
+		}
+		Messages mess=new Messages();
+		for (MessageDTO m : messages.getMessages()) {
+			if(m.getEmailAddress().equals(primalac)) {
+				mess.getMessages().add(m);;
+			}
+		}
+		return mess;
+	}
+}
