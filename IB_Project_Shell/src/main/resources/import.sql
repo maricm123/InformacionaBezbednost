30067eeaf1a459a85f697909433baf183c804ab7
maricm123 <mihailomaric001@gmail.com>
2021-09-20 11:01:01 +0200

    added ib-keystore to data

--- IB_Project_Shell/src/main/resources/import.sql
+++ IB_Project_Shell/src/main/resources/import.sql
@@ -4,12 +4,12 @@
 
 -- Obe lozinke su hesovane pomocu BCrypt algoritma
 
-INSERT INTO USERS (id, email, password, certificate, active, last_password_reset_date) VALUES (1, 'mare@gmail.com', '$2a$12$ntDX3IO4sCSJFU39VjvsVOMIowQK3wVy9UmGS.Vvtr/lP.OWaMcne', './data/' ,true, '2017-10-01 21:58:58.508-07');
-INSERT INTO USERS (id, email, password, certificate, active, last_password_reset_date) VALUES (2, 'niko@gmail.com', '$2a$12$spkA99drHKsDVv8Tsrudpu9o0x.wZaOGBlBan14eUsEZcw62lAzO2', './data/' ,true, '2017-10-01 18:57:58.508-07');
+INSERT INTO users (id, email, password, certificate, active, last_password_reset_date) VALUES (1, 'mare@gmail.com', '$2a$12$ntDX3IO4sCSJFU39VjvsVOMIowQK3wVy9UmGS.Vvtr/lP.OWaMcne', './data/' ,true, '2017-10-01 21:58:58.508-07');
+INSERT INTO users (id, email, password, certificate, active, last_password_reset_date) VALUES (2, 'niko@gmail.com', '$2a$12$spkA99drHKsDVv8Tsrudpu9o0x.wZaOGBlBan14eUsEZcw62lAzO2', './data/' ,true, '2017-10-01 18:57:58.508-07');
 
-INSERT INTO AUTHORITY (id, name) VALUES (1, 'ROLE_REGULAR');
-INSERT INTO AUTHORITY (id, name) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO authority (id, name) VALUES (1, 'ROLE_REGULAR');
+INSERT INTO authority (id, name) VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (1, 1);
-INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (2, 1);
-INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (2, 2);
+INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
+INSERT INTO user_authority (user_id, authority_id) VALUES (2, 1);
+INSERT INTO user_authority (user_id, authority_id) VALUES (2, 2);
